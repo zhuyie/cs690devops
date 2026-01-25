b41a67bd04552ea5deecba9e671438ef2e4bdb6b
Zhuyie <zhuyie@gmail.com>
2026-01-25 01:38:20 +0000

    Initial commit

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,13 @@
+﻿namespace FactorialLibrary.Tests;
+
+using FactorialLibrary;
+
+public class FactorialServiceTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = FactorialService.Factorial(0);
+        Assert.Equal(1,result);
+    }
+}
---
+++ FactorialLibrary/FactorialService.cs
@@ -0,0 +1,11 @@
+﻿namespace FactorialLibrary;
+
+public class FactorialService
+{
+    public static int Factorial(int n) {
+        if (n==1 || n==0) {
+            return 1;
+        }
+        return n * Factorial(n-1);
+    }
+}
