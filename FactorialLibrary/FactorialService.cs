6c92b9492ffa164954f684e8dc225bcd1c9171fe
Zhuyie <zhuyie@gmail.com>
2026-01-25 01:55:41 +0000

    Handle negative numbers

--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -3,6 +3,9 @@
 public class FactorialService
 {
     public static int Factorial(int n) {
+        if (n<0) {
+            throw new InvalidOperationException("Negative numbers don't have proper factorial");
+        }
         if (n==1 || n==0) {
             return 1;
         }
